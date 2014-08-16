75c493338a57ebf4711a71f88f1ef52f60f5a749
drewminns <drew.minns@me.com>
2014-08-16 15:19:45 -0400

    Updates

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,42 +1,40 @@
 var anatomy = {};
 
 anatomy.details = [
-		{ selector: '.value', hint: 'Value', position: 'bottom'},
-		{ selector: '.equal', hint: 'Has the value of', position: 'right'},
-		{ selector: '.data-string', hint: 'String', position: 'right'},
-		{ selector: '.data-integer', hint: 'Integer', position: 'right'},
-		{ selector: '.data-boolean', hint: 'Boolean', position: 'right'},
-		{ selector: '.data-array', hint: 'Array', position: 'top'},
-		{ selector: '.data-object', hint: 'Object', position: 'top'},
-		{ selector: '.object-name', hint: 'Property Name', position: 'bottom'},
-		{ selector: '.object-value', hint: 'Property Value', position: 'bottom'},
-		{ selector: '.declare', hint: 'Declare the construct', position: 'right'},
-		{ selector: '.name', hint: 'Name the construct', position: 'right'},
-		{ selector: '.parameters', hint: 'Create options for parameters', position: 'right'},
-		{ selector: '.ref', hint: 'Call the function', position: 'right'},
-		{ selector: '.callVar', hint: 'Call Variable', position: 'right'},
-		{ selector: '.arg', hint: 'Pass arguments into the parameters', position: 'right'},
-		{ selector: '.block', hint: 'Block Statement', position: 'top'},
-		{ selector: '.output', hint: 'Output', position: 'right'},
-		{ selector: '.compOperator', hint: 'Computational Operator', position: 'right'},
-		{ selector: '.comparisonOperator', hint: 'Comparison Operator', position: 'right'}
+		{ selector: '.string', hint: 'String', position: 'bottom'},
+		{ selector: '.number', hint: 'Number', position: 'bottom'},
+		{ selector: '.boolean', hint: 'Boolean', position: 'bottom'},
+		{ selector: '.literal', hint: 'Boolean', position: 'bottom'},
+		{ selector: '.keyword', hint: 'Keyword', position: 'bottom'},
+		{ selector: '.argument', hint: 'Argument/Parameter', position: 'bottom'},
+		{ selector: '.function', hint: 'Function', position: 'bottom'},
+		{ selector: '.operator', hint: 'Computational Operator', position: 'bottom'},
+		{ selector: '.logicalOperator', hint: 'Logical Operator', position: 'bottom'},
+		{ selector: '.setValue', hint: 'Set a value of', position: 'bottom'},
+		{ selector: '.variable', hint: 'Variable Name', position: 'bottom'},
+		{ selector: '.compareOp', hint: 'Comparison Operator', position: 'bottom'},
+		{ selector: '.variable', hint: 'Variable', position: 'bottom'},
 ];
 
-anatomy.init = function(data) {
+anatomy.init = function() {
 	anatomy.putStuff(anatomy.details);
-}
+};
 
 anatomy.putStuff = function(items) {
 	$.each(items, function(i, piece){
 		anatomy.addHint(piece.selector, piece.hint, piece.position);
 	});
-}
+};
 
 anatomy.addHint = function(span, hint, location) {
 	$(span).addClass('hint--' + location + ' hint--bounce');
 	$(span).attr('data-hint', hint);
-}
+};
+
 
 $(function() {
-	anatomy.init();
+	setTimeout(function() {
+	  anatomy.init();
+	}, 1000);
+
 });
